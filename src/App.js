65b80efaf2e4e3b85cce2b9df3b32f1820134c0e
khairekan-amit <amitkhairekan@gmail.com>
2022-02-26 21:52:41 +0530

    forget to add app file

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      Each branch has hooks implimentation, so check all
+       the branch and checkout whick hooks you want to study
+    </div>
+  )
+}
