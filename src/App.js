45526baca42ba58a75924d8dc8de0c290c5bcf41
amit-dhaterwal <amitdhaterwal12@gmail.com>
2022-03-01 23:22:41 +0530

    simple example with useReducer to unserstand the syntex. Next lets make todo app to better understand why we need useReducer hook.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect, useReducer, Fragment} from 'react'
+
+function init(intialState)  {
+  return {count: intialState.count} 
+}
+
+function Counter({intialState}) {
+  //useReducer(reducer,intialState, init) => [state, dispatch]
+  // reducer(state, action) => newState
+  // action({type: '', payload // optional : })
+  console.log(intialState.count)
+  const [state, dispatch] = useReducer(reducer, intialState, init );
+
+  function demo(state,action) {
+    return {count: state.count - action.payload}
+  }
+
+  function reducer (state,action) {
+    switch (action.type) {
+      case 'increment':
+        (function () {
+          console.log("hello")
+          return {count: state.count + action.payload}
+        })()
+      case 'decrement': 
+          // demo(state,action)
+        return {count: state.count - action.payload}
+      case 'reset': 
+        return init(action.payload)
+      default: 
+        throw new Error();
+    }
+  }
+
+  return (
+    <Fragment>
+      <div >
+        {state.count}
+      </div>
+      <button onClick={() => dispatch({type: 'increment', payload: 2})}>increment</button>
+      <button onClick={() => dispatch({type: 'decrement', payload: 2})}>decrement</button>
+      <button onClick={() => dispatch({type: 'reset', payload: intialState})}>reset</button>
+    </Fragment>
+  )
+}
 
 export default function App() {
   return (
-    <div>
-      Each branch has hooks implimentation, so check all
-       the branch and checkout whick hooks you want to study
-    </div>
+    <Counter intialState={{count : 10, "name": "Amit"}} />
   )
 }
