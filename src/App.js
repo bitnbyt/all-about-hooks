5bf34f682c1596435599e5e61a40c9c288b289eb
amit-dhaterwal <amitdhaterwal12@gmail.com>
2022-03-03 15:06:52 +0530

    added hook flow image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ export default function App() {
   return (
     <div>
       Each branch has hooks implimentation, so check all
-       the branch and checkout whick hooks you want to study
+       the branch and checkout whick hooks you want to study.<br/>
+
+
+
+       <h3>If you want to visulize the hook flow :</h3>
+
+       <a  target="_blank" href="https://github.com/donavon/hook-flow">Hooks flow</a>
+       <br />
+
+       <footer>In case the above repositry doesn't exit there is a "hook-flow.jpg" 
+       image which i have cloned from the above repo.</footer>
+
+       <br />
+
+       <p>Also please don't mind as i have not styled the component as the goal was to learn hooks. </p>
     </div>
   )
 }
