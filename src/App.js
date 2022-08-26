eb63c6575711375d3f55095bb3f44066ba5e90a7
dhaterwal-amit <amit.dhaterwal@aviso.com>
2022-08-26 16:56:27 +0530

    app.js lazygit learning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
 export default function App() {
-  return (
-    <div>
-      Each branch has hooks implimentation, so check all
-       the branch and checkout whick hooks you want to study.<br/>
+	return (
+		<div>
+			Each branch has hooks implimentation, so check all
+			the branch and checkout whick hooks you want to study.<br />
 
 
 
-       <h3>If you want to visulize the hook flow :</h3>
+			<h3           >If you want to visulize the hook flow :</h3>
 
-       <a  target="_blank" href="https://github.com/donavon/hook-flow">Hooks flow</a>
-       <br />
+			<a target="_blank" href="https://github.com/donavon/hook-flow">Hooks flow</a>
+			<br />
 
-       <footer>In case the above repositry doesn't exit there is a "hook-flow.jpg" 
-       image which i have cloned from the above repo.</footer>
+			<          footer>In case the above repositry doesn't exit there is a "hook-flow.jpg"
+				image which i have cloned from the above repo.</footer>
 
-       <br />
+			<br />
 
-       <p>Also please don't mind as i have not styled the component as the goal was to learn hooks. </p>
+			<p>Also please don't mind as i have not styled the component as the goal was to learn hooks. </p>
 
-       <p> Will keep update with remaining hooks as well as some questionary about the hooks.</p>
-    </div>
-  )
+			<p> Will keep update with remaining hooks as well as some questionary about the hooks.</p>
+		</div>
+	)
 }
